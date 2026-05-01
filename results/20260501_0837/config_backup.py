00e639b0f6e9702a0175039b7647e645237a8234
Hamu132 <guba06120@gmail.com>
2026-05-01 10:13:22 +0900

    コンフィグの調整

--- results/20260501_0837/config_backup.py
+++ results/20260501_0837/config_backup.py
@@ -4,13 +4,16 @@ class Config:
     EPOCHS = 5
     LR = 1e-4
     DATASET = "FashionMNIST"
-
+    CLASS_NAMES = [
+        'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
+    ]
+    N_CLASSES = 10
     # --- モデル（ViT）の構造設定 ---
     #IMAGE_SIZEはPATCH_SIZEで割り切れる必要がある
     IMAGE_SIZE = 28
     PATCH_SIZE = 28
     CHANNELS = 1
-    N_CLASSES = 10
     
     DIM = 128
     DEPTH = 6
--- src/config.py
+++ src/config.py
@@ -10,13 +10,13 @@ class Config:
         'T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
         'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'
     ]
+    N_CLASSES = 10
 
     # --- モデル（ViT）の構造設定 ---
     #IMAGE_SIZEはPATCH_SIZEで割り切れる必要がある
     IMAGE_SIZE = 28
     PATCH_SIZE = 28
     CHANNELS = 1
-    N_CLASSES = 10
     
     DIM = 128
     DEPTH = 6
